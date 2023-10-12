a8eef0801c688e7a15e83426bf0b79bfacf64b12
ArturoJesusHuari <115654291+ArturoJesusHuari@users.noreply.github.com>
2023-10-12 13:00:58 -0500

    0.002 problem in funtion destruction

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class Tetriz{
         this.score = 0;
         this.level = 1;
         this.gameOver = false;
-        this.generateShape();
+        this.shape = this.generateShape();
     }
     generateShape(){
         var shape;
@@ -24,29 +24,41 @@ class Tetriz{
         else if(numAleatorio==4){
             shape = new L();
         }
-        this.shape = shape;
+        return shape;
     }
     moveShape(displacementX){
-        if (this.tetrizMatrix.validationPosition(this.shape, 0, displacementX)) {
-            this.shape.moveThroughX(displacementX);
+        if (this.tetrizMatrix.validationPosition(this.shape, this.shape.moveThroughX(displacementX))) {
+            this.shape.setNewCoordinates(this.shape.moveThroughX(displacementX));
+            this.tetrizMatrix.changeMatrix(this.shape);
+            this.tetrizMatrix.draw();
+        }
+    }
+    fallFastShape(){
+        while(this.tetrizMatrix.validationPosition(this.shape,this.shape.fall())){
+            this.shape.setNewCoordinates(this.shape.fall());
             this.tetrizMatrix.changeMatrix(this.shape);
             this.tetrizMatrix.draw();
         }
     }
     roundShape(){
-        this.shape.round();
-        this.tetrizMatrix.changeMatrix(this.shape);
-        this.tetrizMatrix.draw();
+        if (this.tetrizMatrix.validationPosition(this.shape, this.shape.round())){
+            this.shape.setNewCoordinates(this.shape.round());
+            this.tetrizMatrix.changeMatrix(this.shape);
+            this.tetrizMatrix.draw();
+        }
     }
     run(){
         this.tetrizMatrix.draw();
-        if(this.tetrizMatrix.validationPosition(this.shape,1,0)){
-            this.shape.fall();
+        if(this.tetrizMatrix.validationPosition(this.shape,this.shape.fall())){
+            this.shape.setNewCoordinates(this.shape.fall());
             this.tetrizMatrix.changeMatrix(this.shape);
         }else{
-            alert('algo choco '+this.shape.getCoordinates());
-            this.tetrizMatrix.destruccion()
-            this.generateShape();
+            this.tetrizMatrix.destruccion();
+            if(this.tetrizMatrix.validationPosition(this.generateShape(),this.generateShape().getCoordinates())){
+                this.shape = this.generateShape();
+            }else{
+                alert('GAME OVER');
+            }
         }
     }
 }
@@ -65,17 +77,16 @@ class TetrizMatrix{
         this.arrayFullZeros = [0,0,0,0,0,0,0,0,0,0];
     }
     getMatrix(){return this.matrix;}
-    validationPosition(shape,y,x){
+    validationPosition(shape, newCoordinates){
         var coordinates = shape.getCoordinates();
         for(let index=0;index<4;index++){
-            //limits of matrix
-            if(coordinates[index][0]+y == this.matrix.length 
-                || coordinates[index][1]+x == -1
-                || coordinates[index][1]+x == this.matrix[0].length){
+            if(newCoordinates[index][0] == this.matrix.length 
+                || newCoordinates[index][1] == -1
+                || newCoordinates[index][1] == this.matrix[0].length){
                     return false;
                 }
-            if(!coordinates.some(arr => JSON.stringify(arr) === JSON.stringify([coordinates[index][0]+y,coordinates[index][1]+x]))){
-                if(this.matrix[coordinates[index][0]+y][coordinates[index][1]+x]==1){
+            if(!coordinates.some(coordinate => JSON.stringify(coordinate) === JSON.stringify([newCoordinates[index][0],newCoordinates[index][1]]))){
+                if(this.matrix[newCoordinates[index][0]][newCoordinates[index][1]]==1){
                     return false;
                 }
             }
@@ -87,6 +98,7 @@ class TetrizMatrix{
             if(JSON.stringify(this.matrix[index]) == JSON.stringify(this.arrayFullOne)){
                 this.matrix.splice(index,1);
                 this.matrix.unshift(this.arrayFullZeros);
+                alert(this.matrix);
                 this.draw();
             }
         }
@@ -127,128 +139,185 @@ class Shape {
         this.coordinatesNow[3] = coordinates4;
         this.coordinatesBefore = [];
     }
+    getCoordinatesBefore() {return this.coordinatesBefore;}
     getCoordinates(){return this.coordinatesNow;}
     setCoodinatesBefore(){
         for (let index = 0; index < 4; index++) {
             this.coordinatesBefore[index]=[this.coordinatesNow[index][0],this.coordinatesNow[index][1]];
         }
     }
-    getCoordinatesBefore() {return this.coordinatesBefore;}
-    fall(){
+    setNewCoordinates(newCoordinates){
         this.setCoodinatesBefore();
         for (let index = 0; index < 4; index++) {
-            this.coordinatesNow[index][0]+=1;
+            this.coordinatesNow[index]=[newCoordinates[index][0],newCoordinates[index][1]];
         }
     }
-    moveThroughX(displacementX){
-        this.setCoodinatesBefore();
+    fall(){
+        let newCoordinates=[[],[],[],[]]
         for (let index = 0; index < 4; index++) {
-            this.coordinatesNow[index][1]+=displacementX;
+            newCoordinates[index]=[this.coordinatesNow[index][0]+1,this.coordinatesNow[index][1]];
         }
+        return newCoordinates;
+    }
+    moveThroughX(displacementX){
+        let newCoordinates=[[],[],[],[]]
+        for (let index = 0; index < 4; index++) {
+            newCoordinates[index]=[this.coordinatesNow[index][0],this.coordinatesNow[index][1]+displacementX];
+        }
+        return newCoordinates;
     }
 }
 class I extends Shape{
     //Y,X
     constructor() {
-        let firstCoordinateX = Math.round(Math.random()*9)
+        let firstCoordinateX = Math.round(Math.random()+4)
         super([0,firstCoordinateX],
             [1,firstCoordinateX],
             [2,firstCoordinateX],
             [3,firstCoordinateX]);
     }
     round(){
-        this.setCoodinatesBefore();
+        let newCoordinates=[[],[],[],[]]
         if(this.coordinatesNow[0][1]==this.coordinatesNow[3][1]){
+            newCoordinates[3]=[this.coordinatesNow[3][0],this.coordinatesNow[3][1]];
             if(this.coordinatesNow[3][1]<=4){
                 for(let index=0; index<3; index++){
-                    this.coordinatesNow[index]=[this.coordinatesNow[3][0],this.coordinatesNow[3][1]+3-index];
+                    newCoordinates[index]=[this.coordinatesNow[3][0],this.coordinatesNow[3][1]+3-index];
                 }
             }else{
                 for(let index=0; index<3; index++){
-                    this.coordinatesNow[index]=[this.coordinatesNow[3][0],this.coordinatesNow[3][1]-3+index];
+                    newCoordinates[index]=[this.coordinatesNow[3][0],this.coordinatesNow[3][1]-3+index];
                 }
             }
         }else{
-            if(this.coordinatesNow[3][1]<=4){
-                for(let index=0; index<3; index++){
-                    this.coordinatesNow[index]=[this.coordinatesNow[3][0]-3+index,this.coordinatesNow[3][1]];
-                }
+            let initShape, finishShape;
+            if(this.coordinatesNow[3][1]<this.coordinatesNow[0][1]){
+                initShape = this.coordinatesNow[3];
+                finishShape = this.coordinatesNow[0];
             }else{
-                this.coordinatesNow[3]=this.coordinatesNow[0];
-                for(let index=0; index<3; index++){
-                    this.coordinatesNow[index]=[this.coordinatesNow[3][0]-3+index,this.coordinatesNow[3][1]];
-                }
+                initShape = this.coordinatesNow[0];
+                finishShape = this.coordinatesNow[3];
+            }
+            if(initShape[1]>=0 && initShape[1]<=2){
+                newCoordinates[3]=initShape;
+            }else if(initShape[1]==3){
+                newCoordinates[3]=[initShape[0]+1,initShape[1]+1];
+            } else if(finishShape[1]==6){
+                newCoordinates[3]=[initShape[0]+1,initShape[1]-1];
+            }else{
+                newCoordinates[3]=finishShape;
+            }
+            for(let index=0; index<3; index++){
+                newCoordinates[index]=[newCoordinates[3][0]-3+index,newCoordinates[3][1]];
             }
         }
+        return newCoordinates;
     }
 }
 class O extends Shape{
     //Y,X
     constructor() {
-        let firstCoordinateX = Math.round(Math.random()*8)
+        let firstCoordinateX = Math.round(Math.random()+4)
         super([2,firstCoordinateX],
             [2,firstCoordinateX+1],
             [3,firstCoordinateX],
             [3,firstCoordinateX+1]);
     }
-    round(){}
+    round(){
+        return this.coordinatesNow;
+    }
 }
 class Z extends Shape{
     //Y,X
     constructor() {
-        let firstCoordinateX = Math.round(Math.random()*7+1)
+        let firstCoordinateX = Math.round(Math.random()+4)
         super([3,firstCoordinateX],
             [3,firstCoordinateX+1],
             [2,firstCoordinateX-1],
             [2,firstCoordinateX]);
     }
     round(){
-        
+        let newCoordinates=[[],[],[],[]]
+        newCoordinates[0]=[this.coordinatesNow[0][0],this.coordinatesNow[0][1]];
+        if(this.coordinatesNow[0][0] == this.coordinatesNow[1][0]){
+            newCoordinates[2]=[this.coordinatesNow[0][0],this.coordinatesNow[0][1]+1];
+            newCoordinates[3]=[this.coordinatesNow[0][0]-1,this.coordinatesNow[0][1]+1];
+            newCoordinates[1]=[this.coordinatesNow[0][0]+1,this.coordinatesNow[0][1]];
+        }else{
+            newCoordinates[2]=[this.coordinatesNow[0][0]-1,this.coordinatesNow[0][1]];
+            newCoordinates[3]=[this.coordinatesNow[0][0]-1,this.coordinatesNow[0][1]-1];
+            newCoordinates[1]=[this.coordinatesNow[0][0],this.coordinatesNow[0][1]+1];
+        }
+        return newCoordinates;
     }
 }
 class W extends Shape{
     //Y,X
     constructor() {
-        let firstCoordinateX = Math.round(Math.random()*7+1)
+        let firstCoordinateX = Math.round(Math.random()+4)
         super([3,firstCoordinateX],
             [2,firstCoordinateX],
             [3,firstCoordinateX+1],
             [3,firstCoordinateX-1]);
     }
     round(){
-        if(this.coordinatesNow[0][1]>0 && this.coordinatesNow[0][1]<9){
-            this.setCoodinatesBefore();
-            this.coordinatesNow[2][0]=this.coordinatesNow[1][0];
-            this.coordinatesNow[2][1]=this.coordinatesNow[1][1];
-            this.coordinatesNow[1][0]=this.coordinatesNow[3][0];
-            this.coordinatesNow[1][1]=this.coordinatesNow[3][1];
-            this.coordinatesNow[3][0]=this.coordinatesNow[0][0]+(this.coordinatesNow[0][0]-this.coordinatesNow[2][0])
-            this.coordinatesNow[3][1]=this.coordinatesNow[0][1]+(this.coordinatesNow[0][1]-this.coordinatesNow[2][1])
-        }
+        let newCoordinates=[[],[],[],[]];
+        newCoordinates[0]=[this.coordinatesNow[0][0],this.coordinatesNow[0][1]];
+        newCoordinates[2][0]=this.coordinatesNow[1][0];
+        newCoordinates[2][1]=this.coordinatesNow[1][1];
+        newCoordinates[1][0]=this.coordinatesNow[3][0];
+        newCoordinates[1][1]=this.coordinatesNow[3][1];
+        newCoordinates[3][0]=this.coordinatesNow[0][0]+(newCoordinates[0][0]-newCoordinates[2][0])
+        newCoordinates[3][1]=this.coordinatesNow[0][1]+(newCoordinates[0][1]-newCoordinates[2][1])
+        return newCoordinates;
     }
 }
 class L extends Shape{
     //Y,X
     constructor() {
-        let firstCoordinateX = Math.round(Math.random()*8+1)
+        let firstCoordinateX = 5;
         super([1,firstCoordinateX],
             [1,firstCoordinateX-1],
-            [2,firstCoordinateX],
+            [2,firstCoordinateX], //centro
             [3,firstCoordinateX]);
     }
     round(){
-        
+        let newCoordinates=[[],[],[],[]];
+        newCoordinates[2]=[this.coordinatesNow[2][0],this.coordinatesNow[2][1]];
+        if(this.coordinatesNow[2][1]==this.coordinatesNow[0][1]){
+            if(this.coordinatesNow[0][0]>this.coordinatesNow[2][0]){
+                newCoordinates[0]=[newCoordinates[2][0],newCoordinates[2][1]+1];
+                newCoordinates[1]=[newCoordinates[2][0]-1,newCoordinates[2][1]+1];
+                newCoordinates[3]=[newCoordinates[2][0],newCoordinates[2][1]-1];
+            }else{
+                newCoordinates[0]=[newCoordinates[2][0],newCoordinates[2][1]-1];
+                newCoordinates[1]=[newCoordinates[2][0]+1,newCoordinates[2][1]-1];
+                newCoordinates[3]=[newCoordinates[2][0],newCoordinates[2][1]+1];
+            }
+        }else{
+            if(this.coordinatesNow[1][0]>this.coordinatesNow[0][0]){
+                newCoordinates[0]=[newCoordinates[2][0]+1,newCoordinates[2][1]];
+                newCoordinates[1]=[newCoordinates[2][0]+1,newCoordinates[2][1]+1];
+                newCoordinates[3]=[newCoordinates[2][0]-1,newCoordinates[2][1]];
+            }else{
+                newCoordinates[0]=[newCoordinates[2][0]-1,newCoordinates[2][1]];
+                newCoordinates[1]=[newCoordinates[2][0]-1,newCoordinates[2][1]-1];
+                newCoordinates[3]=[newCoordinates[2][0]+1,newCoordinates[2][1]];
+            }
+        }
+        return newCoordinates;
     }
 }
 var t = new Tetriz();
 document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowLeft') {
         t.moveShape(-1);
-    } else if (event.key === 'ArrowRight') {
+    }else if (event.key === 'ArrowRight') {
         t.moveShape(1);
-    }
-    else if (event.key === 'ArrowUp') {
+    }else if (event.key === 'ArrowUp') {
         t.roundShape(1);
+    }else if (event.key === 'ArrowDown') {
+        t.fallFastShape();
     }
 });
-setInterval("t.run()", 100);
+setInterval("t.run()", 500);
